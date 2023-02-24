e9a77fa1b327b6368ff05279ffb6043604734644
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-02-23 21:08:54 -0500

    Header added to the main file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include './top-header.html';
+    ?>
+</head>
+
+<body>
+    <?php
+    include './header.html';
+    ?>
+</body>
+
+</html>
