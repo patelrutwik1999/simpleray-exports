731e4050e443e92e435741b93a7a15bcb4ff4bf0
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-02-24 05:19:24 -0500

    admin panel login, signup setup done. Base tag used to solve the problem of getting css to the respected files.

---
+++ admin-panel/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- If need to mention specific title then give it before php include -->
+    <title>Login Page</title>
+    <?php
+    include 'top-header.php';
+    ?>
+</head>
+
+<body class="animsition">
+    <?php
+    include 'admin-login/admin-login.php';
+    include 'sub-footer.php';
+    ?>
+</body>
+
+</html>
