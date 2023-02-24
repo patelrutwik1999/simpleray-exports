20cb12996f6905f4d2ef7b9bb31b4b1e0337089b
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-02-23 21:52:19 -0500

    Categories section added

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     include './header.html';
     include './sliding-checkout/sliding-checkout.html';
     include './sliding-images/sliding-images.html';
+    include './categories-landing-page/categories-landing-page.html';
     include './footer.html';
     include './sub-footer.html';
     ?>
