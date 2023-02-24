c19504fdbea4d0b53c2a7ffef5dd733485927115
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-02-24 00:20:47 -0500

    ADmin panel setup done for its assests, header, footer, and landing page.

---
+++ admin-panel/admin-home.php
@@ -0,0 +1,27 @@
+<html lang="en">
+
+<head>
+    <?php
+    include './header/top-header.html';
+    ?>
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <?php
+        include './header/header.html';
+        include './sidebar/menu-sidebar.html';
+        ?>
+        <div class="page-container">
+            <?php
+            include './header/header-desktop.html';
+            include './landing-page/landing-page.html';
+            include './footer/footer.html';
+            include './footer/sub-footer.html';
+            ?>
+        </div>
+
+    </div>
+</body>
+
+</html>
