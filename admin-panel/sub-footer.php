710002dfc78849845726c5dcd3937f46bfded1d5
Charit_Patel <charit15499@gmail.com>
2023-02-25 17:40:21 -0500

    Error in script loading in admin-panel is resolved

--- admin-panel/sub-footer.php
+++ admin-panel/sub-footer.php
@@ -1,23 +1,23 @@
 <!-- Jquery JS-->
-<script src="vendor/jquery-3.2.1.min.js"></script>
+<script src="admin-panel-assets/vendor/jquery-3.2.1.min.js"></script>
 <!-- Bootstrap JS-->
-<script src="vendor/bootstrap-4.1/popper.min.js"></script>
-<script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<script src="admin-panel-assets/vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="admin-panel-assets/vendor/bootstrap-4.1/bootstrap.min.js"></script>
 <!-- Vendor JS       -->
-<script src="vendor/slick/slick.min.js">
+<script src="admin-panel-assets/vendor/slick/slick.min.js">
 </script>
-<script src="vendor/wow/wow.min.js"></script>
-<script src="vendor/animsition/animsition.min.js"></script>
-<script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+<script src="admin-panel-assets/vendor/wow/wow.min.js"></script>
+<script src="admin-panel-assets/vendor/animsition/animsition.min.js"></script>
+<script src="admin-panel-assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
 </script>
-<script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-<script src="vendor/counter-up/jquery.counterup.min.js">
+<script src="admin-panel-assets/vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="admin-panel-assets/vendor/counter-up/jquery.counterup.min.js">
 </script>
-<script src="vendor/circle-progress/circle-progress.min.js"></script>
-<script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-<script src="vendor/chartjs/Chart.bundle.min.js"></script>
-<script src="vendor/select2/select2.min.js">
+<script src="admin-panel-assets/vendor/circle-progress/circle-progress.min.js"></script>
+<script src="admin-panel-assets/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="admin-panel-assets/vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="admin-panel-assets/vendor/select2/select2.min.js">
 </script>
 
 <!-- Main JS-->
-<script src="js/main.js"></script>
+<script src="admin-panel-assets/js/main.js"></script>
