05faf57502790458eb1a01ac05116f68fae2e5a2
Charit_Patel <charit15499@gmail.com>
2023-02-25 16:42:35 -0500

    copyright in admin panel is changed

--- admin-panel/footer.php
+++ admin-panel/footer.php
@@ -1,7 +1,8 @@
 <footer class="row">
     <div class="col-md-12">
         <div class="copyright">
-            <p>Copyright © 2018 Colorlib. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.
+            <p>Copyright © 2023 Simpleray-Exports. All rights reserved.
+                <!-- <p> Template by <a href="https://colorlib.com">Colorlib</a></p> -->
             </p>
         </div>
     </div>
