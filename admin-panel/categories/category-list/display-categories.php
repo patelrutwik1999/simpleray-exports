8859a8b5c0b11629064649c9212bdcf0ab3bbcb5
Charit_Patel <charit15499@gmail.com>
2023-02-26 03:31:45 -0500

    header-mobile file is added to admin-home page, category list is created and displayed in the table

---
+++ admin-panel/categories/category-list/display-categories.php
@@ -0,0 +1,28 @@
+<html lang="en">
+
+<head>
+    <?php
+    include '../../top-header.php';
+    ?>
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <?php
+        include '../../header-mobile.php';
+        include '../../sidebar/menu-sidebar.html';
+        ?>
+        <div class="page-container">
+            <?php
+            include '../../header-desktop.php';
+            include 'categories-list.php';
+            include '../../footer.php';
+            ?>
+        </div>
+    </div>
+    <?php
+    include '../../sub-footer.php';
+    ?>
+</body>
+
+</html>
