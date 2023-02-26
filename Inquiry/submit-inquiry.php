0039be7f1f6f99c058067a3248b360bf6a848bfb
Charit_Patel <charit15499@gmail.com>
2023-02-26 08:24:12 -0500

    insert categpry form reorganized

---
+++ Inquiry/submit-inquiry.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include 'top-header.php';
+    ?>
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    include 'sliding-checkout/sliding-checkout.html';
+    include 'sliding-images/sliding-images.html';
+    include 'categories-landing-page/categories-landing-page.html';
+    include 'footer.php';
+    include 'sub-footer.php';
+    ?>
+</body>
+
+</html>
--- admin-panel/categories/add-category/add-category-form.php
+++ admin-panel/categories/add-category/add-category-form.php
@@ -2,7 +2,7 @@
     <div class="section__content section__content--p30">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-6">
+                <div style = "margin: 20vh auto;" class="col-lg-6">
                     <div class="card">
                         <div class="card-header">Insert Category</div>
                         <div class="card-body">
@@ -13,14 +13,14 @@
                             <form action="categories/category-list/display-categories.php" method="post" novalidate="novalidate">
                                 <div class="form-group">
                                     <label for="cc-payment" class="control-label mb-1">Category Name</label>
-                                    <input id="cc-pament" name="cc-payment" type="text" class="form-control"
+                                    <input id="cc-pament" name="category_name" type="text" class="form-control"
                                         aria-required="true" aria-invalid="false" value="">
                                 </div>
 
                                 <div class="form-group has-success">
                                     <label for="cc-name" class="control-label mb-1">Is it a sub-category?</label>
                                     <div class="col-20 col-md-20">
-                                        <select name="select" id="select" class="form-control">
+                                        <select name="subcategory-select" id="select" class="form-control">
                                             <option value="2">Please select</option>
                                             <option value="1">Yes</option>
                                             <option value="0">No</option>
@@ -31,7 +31,7 @@
                                     <label for="cc-name" class="control-label mb-1">Please select the name of the parent
                                         category (if applicable)</label>
                                     <div class="col-20 col-md-20">
-                                        <select name="select" id="select" class="form-control" placeholder="Disabled"
+                                        <select name="category-parent-select" id="select" class="form-control" placeholder="Disabled"
                                             disabled="">
                                             <option value="2">Please select</option>
                                             <option value="1">Yes</option>
@@ -40,7 +40,7 @@
                                     </div>
                                 </div>
                                 <div>
-                                    <button id="submit-button" type="submit" class="btn btn-lg btn-info btn-block">
+                                    <button id="submit-button" type="submit" class="btn btn-lg btn-info btn-block" name="submit-button">
                                         <i class="fa fa-arrow-circle-right fa-lg"></i>&nbsp;
                                         <span id="payment-button-amount">Submit</span>
                                         <span id="payment-button-sending" style="display:none;">Sending…</span>
