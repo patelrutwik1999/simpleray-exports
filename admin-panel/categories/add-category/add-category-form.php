8a84c2a82758507e018e402b04bb26e6c5efbe54
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-02-28 22:05:01 -0500

    Added category processing file

--- admin-panel/categories/add-category/add-category-form.php
+++ admin-panel/categories/add-category/add-category-form.php
@@ -10,7 +10,7 @@
                     <div class="card">
                         <div class="card-header insert-category-heading">Category Details</div>
                         <div class="card-body">
-                            <form action="categories/category-list/display-categories.php" method="post" novalidate="novalidate">
+                            <form action="categories/add-category/store-category/store-category.php" method="post" novalidate="novalidate">
                                 <div class="form-group">
                                     <label for="cc-payment" class="control-label mb-1">Category Name</label>
                                     <input id="cc-pament" name="category_name" type="text" class="form-control" aria-required="true" aria-invalid="false" value="">
@@ -19,7 +19,7 @@
                                 <div class="form-group has-success">
                                     <label for="cc-name" class="control-label mb-1">Is it a sub-category?</label>
                                     <div class="col-20 col-md-20">
-                                        <select name="subcategory-select" id="select-sub-category" class="form-control">
+                                        <select name="has_sub_category" id="select-sub-category" class="form-control">
                                             <option value="2">Please select</option>
                                             <option value="1">Yes</option>
                                             <option value="0">No</option>
@@ -30,7 +30,7 @@
                                     <label for="cc-name" class="control-label mb-1">Please select the name of the parent
                                         category (if applicable)</label>
                                     <div class="col-20 col-md-20">
-                                        <select name="category-parent-select" id="select-parent-category" class="form-control" placeholder="Disabled" disabled='false'>
+                                        <select name="parent_category" id="select-parent-category" class="form-control" placeholder="Disabled" disabled='false'>
                                             <option value="2">Please select</option>
                                             <option value="1">Yes</option>
                                             <option value="0">No</option>
