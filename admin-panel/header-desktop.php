dd427bbfbdafed553f0384b2231ffb5deef1fd74
Charit_Patel <charit15499@gmail.com>
2023-03-01 07:35:16 -0500

    search bar added in product list page

--- admin-panel/header-desktop.php
+++ admin-panel/header-desktop.php
@@ -5,7 +5,7 @@
             <div class="header-wrap">
                 <form class="form-header" action="" method="POST">
                     <input class="au-input au-input--xl" type="text" name="search"
-                        placeholder="Search for datas &amp; reports..." />
+                        placeholder="Search here..." />
                     <button class="au-btn--submit" type="submit">
                         <i class="zmdi zmdi-search"></i>
                     </button>
--- admin-panel/products/products-list/products-list.php
+++ admin-panel/products/products-list/products-list.php
@@ -3,10 +3,25 @@
     <hr style="height: 15px; border: 0; box-shadow: inset 0 12px 12px -12px rgba(9, 84, 132);">
     <div class="section__content section__content--p30">
         <div class="container-fluid">
-            <div class="row add-product-button-section">
-                <a href="products/add-product/insert-product.php" class='add-product'>
-                    <i class="zmdi zmdi-plus"></i> Add product
-                </a>
+            <div class="row ">
+                <div>
+                    <div class="products-list-search-bar">
+                        <form class="form-header" action="" method="POST">
+                            <input class="au-input au-input--xl" type="text" name="search"
+                                placeholder="Search product..." />
+                            <button class="au-btn--submit" type="submit">
+                                <i class="zmdi zmdi-search"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div>
+                    <div class="add-product-button-section">
+                        <a href="products/add-product/insert-product.php" class='add-product'>
+                            <i class="zmdi zmdi-plus"></i> Add product
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="row">
                 <div style="margin: 3.5vh auto" class="col-lg-10">
