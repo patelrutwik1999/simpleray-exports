d6bbf42e24f3a380b2bfd7ba4d958d0264ef9643
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-03-07 10:43:09 -0500

    Change config file

--- config/config.php
+++ config/config.php
@@ -3,6 +3,11 @@ $db_host = "us-cdbr-east-06.cleardb.net";
 $db_user = "b8db078d719f48";
 $db_pass = "735c7f9e";
 $db_name = "heroku_4cf325fd1228099";
-echo "hi";
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Connection not established");
-echo "hello";
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    // mysql: //b8db078d719f48:735c7f9e@us-cdbr-east-06.cleardb.net/heroku_4cf325fd1228099?reconnect=true
+    echo "Connection not established";
+} else {
+    echo "Connection successful";
+}
