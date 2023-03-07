08c9ec096015a1c20c6f57fdfa1c93198f420f78
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-03-07 11:20:08 -0500

    remove the else part for successful connection and added die() statement

--- config/config.php
+++ config/config.php
@@ -7,7 +7,5 @@ $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 if ($conn->connect_error) {
     // mysql: //b8db078d719f48:735c7f9e@us-cdbr-east-06.cleardb.net/heroku_4cf325fd1228099?reconnect=true
-    echo "Connection not established";
-} else {
-    echo "Connection successful";
+    die("Connection not established");
 }
