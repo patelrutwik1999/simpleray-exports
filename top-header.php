a202a73565585d8e1380bb668324cc94a97cd201
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-03-07 10:37:06 -0500

    remove header for CORS from mail header

--- top-header.php
+++ top-header.php
@@ -1,6 +1,6 @@
 <!-- <base href="http://localhost/simpleray-exports/"> -->
 <base href="https://simplerayexports.herokuapp.com/">
-<?php header("Access-Control-Allow-Origin: *"); ?>
+
 <meta charset="UTF-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
