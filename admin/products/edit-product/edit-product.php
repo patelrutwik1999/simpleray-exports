718f1df385d0b807065138601baf7b78df816440
Charit_Patel <charit15499@gmail.com>
2023-03-08 09:22:33 -0500

    Edit product setup

---
+++ admin/products/edit-product/edit-product.php
@@ -0,0 +1,68 @@
+<html lang="en">
+
+<head>
+    <!-- Title Page-->
+    <title>Edit Product</title>
+
+    <?php
+    session_start();
+    include '../../../config/config.php';
+    include '../../top-header.php';
+    ?>
+    <!-- DataTables -->
+    <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/libs/datatables.net-autoFill-bs4/css/autoFill.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
+    <link href="assets/libs/datatables.net-keytable-bs4/css/keyTable.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
+
+    <!-- Responsive datatable examples -->
+    <link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
+
+</head>
+
+<body>
+    <div id="layout-wrapper">
+
+        <?php
+        include '../../header.php';
+        include '../../sidebar/menu-sidebar.php';
+        ?>
+        <div class="main-content">
+            <?php
+            include 'edit-product-form.php';
+            include '../../footer.php';
+            ?>
+        </div>
+        <!-- end main content-->
+    </div>
+    <?php
+    include '../../sub-footer.php';
+    ?>
+    <!-- Table Editable plugin -->
+    <script src="assets/libs/table-edits/build/table-edits.min.js"></script>
+
+    <script src="assets/js/pages/table-editable.init.js"></script>
+
+    <!-- Required datatable js -->
+    <script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <!-- Buttons examples -->
+    <script src="assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+    <script src="assets/libs/jszip/jszip.min.js"></script>
+    <script src="assets/libs/pdfmake/build/pdfmake.min.js"></script>
+    <script src="assets/libs/pdfmake/build/vfs_fonts.js"></script>
+    <script src="assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="assets/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
+    <!-- Responsive examples -->
+    <script src="assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+
+    <!-- Datatable init js -->
+    <script src="assets/js/pages/datatables.init.js"></script>
+</body>
+
+</html>
