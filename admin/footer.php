a73d8b886578b41a3d00c6147e4b51d161c9b519
Charit_Patel <charit15499@gmail.com>
2023-03-08 06:09:41 -0500

    product info is fetched from the database and displayed in the table

--- admin/footer.php
+++ admin/footer.php
@@ -4,11 +4,6 @@
             <div class="col-sm-6">
                 <script>document.write(new Date().getFullYear())</script> © Simpleray Exports.
             </div>
-            <!-- <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
-                </div>
-            </div> -->
         </div>
     </div>
 </footer>
