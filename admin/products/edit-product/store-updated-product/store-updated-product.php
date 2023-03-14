585f32c4f16743f840856b8bda48f601c8d6a467
Charit_Patel <charit15499@gmail.com>
2023-03-13 20:02:53 -0400

    Merge conflict resolved

--- admin/products/edit-product/store-updated-product/store-updated-product.php
+++ admin/products/edit-product/store-updated-product/store-updated-product.php
@@ -26,21 +26,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $categories = mysqli_fetch_array($retrieved_categories);
     $parentCategoryName = $categories['category_name'];
 
-    //Check the product name already exists or not. Because product name has a unique key index.
-    $get_single_product = "select * from products where product_name = '$productName'";
-    $single_product = mysqli_query($conn, $get_single_product);
-    $num = mysqli_num_rows($single_product);
-    $row = $single_product->fetch_assoc();
+    // //Check the product name already exists or not. Because product name has a unique key index.
+    // $get_single_product = "select * from products where product_name = '$productName'";
+    // $single_product = mysqli_query($conn, $get_single_product);
+    // $num = mysqli_num_rows($single_product);
+    // $row = $single_product->fetch_assoc();
 
-    //$num = 1 means that product name already exists.
-    if ($num == 1) {
-        $_SESSION['is-error'] = true;
-        $_SESSION['message'] = true;
-        $_SESSION['error-message'] = "The product name already exist. Please change the product name.";
-        header("location:../edit-product-form-base.php?product_id=$productId");
-    } else {
-        if ($changePhoto == '0') {
-            $update_product = "update products set
+    // //$num = 1 means that product name already exists.
+    // if ($num == 1) {
+    //     $_SESSION['is-error'] = false;
+    //     $_SESSION['message'] = true;
+    //     $_SESSION['error-message'] = "The product name already exist. Please change the product name.";
+    //     header("location:../edit-product-form-base.php?product_id=$productId");
+    // } else {
+    if ($changePhoto == '0') {
+        $update_product = "update products set
             product_name = '$productName',
             description = '$productDescription',
             price = '$productPrice',
@@ -50,42 +50,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             where product_id = '$productId'
             ";
 
-            if (mysqli_query($conn, $update_product)) {
-                mysqli_close($conn);
-                $_SESSION['is-error'] = false;
-                $_SESSION['message'] = true;
-                $_SESSION['success-message'] = "The product has been updated.";
-                header("location:../edit-product-form-base.php?product_id=$productId");
-            } else {
-                mysqli_close($conn);
-                $_SESSION['is-error'] = true;
-                $_SESSION['message'] = true;
-                $_SESSION['error-message'] = "The product has not been updated.";
-                header("location:../edit-product-form-base.php?product_id=$productId");
-            }
+        if (mysqli_query($conn, $update_product)) {
+            mysqli_close($conn);
+            $_SESSION['is-error'] = false;
+            $_SESSION['message'] = true;
+            $_SESSION['success-message'] = "The product has been updated.";
+            header("location:../edit-product-form-base.php?product_id=$productId");
         } else {
-            //Uploads the file at specific location and stores the path info in the database
-            $target_dir = "../../../assets/product-images/";
-            $target_file = $target_dir . basename($_FILES["new_photo"]["name"]);
+            mysqli_close($conn);
+            $_SESSION['is-error'] = true;
+            $_SESSION['message'] = true;
+            $_SESSION['error-message'] = "The product has not been updated.";
+            header("location:../edit-product-form-base.php?product_id=$productId");
+        }
+    } else {
+        //Uploads the file at specific location and stores the path info in the database
+        $target_dir = "../../../assets/product-images/";
+        $target_file = $target_dir . basename($_FILES["new_photo"]["name"]);
 
-            $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-            $image = basename($_FILES["new_photo"]["name"], $imageFileType); // used to store the filename in a variable
-            $imageFileType = strtolower($imageFileType);
+        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+        $image = basename($_FILES["new_photo"]["name"], $imageFileType); // used to store the filename in a variable
+        $imageFileType = strtolower($imageFileType);
 
-            if ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png") {
+        if ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png") {
 
-                mysqli_close($conn);
-                $_SESSION['is-error'] = true;
-                $_SESSION['message'] = true;
-                $_SESSION['error-message'] = "File format is not supported.";
-                header("location:../edit-product-form-base.php?product_id=$productId");
+            mysqli_close($conn);
+            $_SESSION['is-error'] = true;
+            $_SESSION['message'] = true;
+            $_SESSION['error-message'] = "File format is not supported.";
+            header("location:../edit-product-form-base.php?product_id=$productId");
 
-            } else {
-                if (move_uploaded_file($_FILES["new_photo"]["tmp_name"], $target_file)) {
+        } else {
+            if (move_uploaded_file($_FILES["new_photo"]["tmp_name"], $target_file)) {
 
-                    $filePathForHtml = substr($target_file, 9);
+                $filePathForHtml = substr($target_file, 9);
 
-                    $update_product = "update products set
+                $update_product = "update products set
                     product_name = '$productName',
                     description = '$productDescription',
                     price = '$productPrice',
@@ -96,30 +96,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     where product_id = '$productId'
                     ";
 
-                    if (mysqli_query($conn, $update_product)) {
-                        mysqli_close($conn);
-                        $_SESSION['is-error'] = false;
-                        $_SESSION['message'] = true;
-                        $_SESSION['success-message'] = "The product has been updated.";
-                        header("location:../edit-product-form-base.php?product_id=$productId");
-                    } else {
-                        mysqli_close($conn);
-                        $_SESSION['is-error'] = true;
-                        $_SESSION['message'] = true;
-                        $_SESSION['error-message'] = "The product has not been updated.";
-                        header("location:../edit-product-form-base.php?product_id=$productId");
-                    }
+                if (mysqli_query($conn, $update_product)) {
+                    mysqli_close($conn);
+                    $_SESSION['is-error'] = true;
+                    $_SESSION['message'] = true;
+                    $_SESSION['success-message'] = "The product has been updated.";
+                    header("location:../edit-product-form-base.php?product_id=$productId");
                 } else {
                     mysqli_close($conn);
                     $_SESSION['is-error'] = true;
                     $_SESSION['message'] = true;
-                    $_SESSION['error-message'] = "Error is occurred in uploading image. Please try again.";
+                    $_SESSION['error-message'] = "The product has not been updated.";
                     header("location:../edit-product-form-base.php?product_id=$productId");
                 }
+            } else {
+                mysqli_close($conn);
+                $_SESSION['is-error'] = true;
+                $_SESSION['message'] = true;
+                $_SESSION['error-message'] = "Error is occurred in uploading image. Please try again.";
+                header("location:../edit-product-form-base.php?product_id=$productId");
             }
         }
     }
 }
+
 // //Checks the photo has been changed or not.
 // //Fetches photo from database.
 // $findProductPhoto = "select photo from products where product_id = '$productId'";
@@ -136,23 +136,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 //     if ($uploadedPhotoLastIndex == strlen($filePathForHtml) - 1) {
 //         $filePathForHtml = $productCurrentDatabasePhoto;
 //     }
-// }
-
-
-// if (mysqli_query($conn, $update_product)) {
-//     mysqli_close($conn);
-//     $_SESSION['is-error'] = false;
-//     $_SESSION['message'] = true;
-//     $_SESSION['success-message'] = "The product has been updated.";
-//     echo "Success";
-//     header("location:../edit-product-form-base.php?product_id=$productId");
-// } else {
-//     mysqli_close($conn);
-//     $_SESSION['is-error'] = true;
-//     $_SESSION['message'] = true;
-//     $_SESSION['error-message'] = "The category has not been updated.";
-//     echo "Failure";
-//     header("location:../edit-product-form-base.php?product_id=$productId");
 // }
 
 ?>
